314e011867a9de0cb3b7e190ab4c341a9de4bb9f
Leandro Torres <leandrohenrique_@live.com>
2024-07-13 18:18:30 -0300

    add: mensagem

--- server/src/Schema/index.ts
+++ server/src/Schema/index.ts
@@ -1,3 +1,6 @@
+/* -------------- GERENCIA E DISPONIBILIZA TODAS AS QUERIES E MUTATIONS DO PROJETO -------------- */
+/* ------- PARA ADD UMA QUERIE OU MUTATION NOVA EH SO COLOCAR O NOME DELA DENTRO DE FIELD ------- */
+
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
 
 const RootQuery = new GraphQLObjectType({
