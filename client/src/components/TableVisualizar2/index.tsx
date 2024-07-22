d7d5ba757c399c67a83e06cd6afcfc1d20daf613
leandroTorres <leandrohenrique_@live.com>
2024-07-22 12:42:37 -0300

    finalizei minha table

--- client/src/components/TableVisualizar2/index.tsx
+++ client/src/components/TableVisualizar2/index.tsx
@@ -1,22 +1,49 @@
-import { red } from "@mui/material/colors";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { IconButton } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+
+function handleEdit(data: any){
+    console.log(data.row)
+}
+
+function handleDelete(data: any){
+    console.log(data)
+}
+
 
 export default function TableVisualizar2() {
 
-
     const columns: GridColDef[] = [
         { field: 'id', headerName: 'ID', width: 70},
-        { field: 'firstName', headerName: 'First name', width: 130, headerAlign: 'center', align: 'center',},
-        { field: 'lastName', headerName: 'Last name', width: 130, headerAlign: 'center', align: 'center' },
-        { field: 'age', headerName: 'Age', type: 'number', width: 90, headerAlign: 'center', align: 'center'},
+        { field: 'firstName', headerName: 'First name', flex: 1, headerAlign: 'center', align: 'center'},
+        { field: 'lastName', headerName: 'Last name', flex: 1 , headerAlign: 'center', align: 'center' },
+        { field: 'age', headerName: 'Age', flex: 1, type: 'number', headerAlign: 'center', align: 'center'},
         {
             field: 'fullName', 
             headerName: 'Full name',
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
-            width: 160,
+            flex: 1,
             headerAlign: 'center', align: 'center',
             valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: (params: GridRenderCellParams) => (
+                <div>
+                    <IconButton onClick={() => handleEdit(params)} color="inherit">
+                        <EditIcon sx={{fontSize: '80%'}}/>
+                    </IconButton>
+                    <IconButton onClick={() => handleDelete(params)} color="inherit">
+                        <DeleteIcon sx={{fontSize: '80%'}} />
+                    </IconButton>
+                </div>
+            ),
         },
       ];
 
@@ -36,12 +63,15 @@ export default function TableVisualizar2() {
     ]
 
     return (
-        <div style={{ height: '100%' , width: '100%' }}>
+        <div className= "h-[95%] w-[100%]">
+            <div className= "text-center mb-2">
+                <h2>TABELA DADOS</h2>
+            </div>
             <DataGrid
-
                 rows={rows}
                 columns={columns}
                 disableColumnResize
+                rowHeight={40}
                 sx={{
                     boxShadow: 3,
                     border: 0,
@@ -59,7 +89,7 @@ export default function TableVisualizar2() {
                 },
                 }}
                 pageSizeOptions={[10, 20]}
-                checkboxSelection
+                // checkboxSelection
             />
         </div>
     )
